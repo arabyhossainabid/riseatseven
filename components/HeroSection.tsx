a91df7afc77e1778e5137ea1e1e0c46e2ec1edaa
arabyhossainabid <arabyhossainabid@gmail.com>
2026-05-07 23:27:56 +0600

    fix: import useState in HeroSection

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import Link from "next/link";
 import Image from "next/image";
