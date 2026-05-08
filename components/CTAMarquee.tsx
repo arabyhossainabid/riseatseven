0d77f03f308eef60578ce8d9ed1ca2e4364b95f6
arabyhossainabid <arabyhossainabid@gmail.com>
2026-05-08 20:17:37 +0600

    refactor: update ScrollTrigger import to default export in CTAMarquee component

--- components/CTAMarquee.tsx
+++ components/CTAMarquee.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import Link from "next/link";
 
 gsap.registerPlugin(ScrollTrigger);
